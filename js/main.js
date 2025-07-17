233f862ef945c940e251f1b031ab8722dca11940
Darwin Gabriel <darwin.g.a.f@hotmail.com>
2025-07-17 13:43:39 -0300

    change navbar styleand add info card section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 let menu = document.querySelector(".menu");
+let hamburguer = document.querySelector(".hamburguer");
 
 document.querySelector("#btn-menu").addEventListener("click", () => {
     menu.classList.toggle("active");
+
+    if (menu.classList.contains("active")) {
+        hamburguer.innerHTML = "X";
+    } else {
+        hamburguer.innerHTML = "&#9776;";
+    }
 });
