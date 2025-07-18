7256cac54c0c7a2c5f1b32453b9d2d0c76d5b292
Darwin Gabriel <darwin.g.a.f@hotmail.com>
2025-07-18 13:40:31 -0300

    finish mobile first

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector(".menu");
 let hamburguer = document.querySelector(".hamburguer");
+let link = document.querySelectorAll(".menu li a");
 
 document.querySelector("#btn-menu").addEventListener("click", () => {
     menu.classList.toggle("active");
@@ -9,4 +10,24 @@ document.querySelector("#btn-menu").addEventListener("click", () => {
     } else {
         hamburguer.innerHTML = "&#9776;";
     }
+});
+
+link.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamburguer.innerHTML = "&#9776;";
+        menu.classList.remove("active");
+    });
+});
+
+document.querySelectorAll("details").forEach((item) => {
+    item.addEventListener("toggle", (item) => {
+        const details = item.target;
+        if (details.open) {
+            details.style.backgroundColor = "#1b1b1b";
+            details.style.color = "white";
+        } else {
+            details.style.backgroundColor = "white";
+            details.style.color = "black";
+        }
+    });
 });
